341160d5a0fd5a0c0c62794028ca8e76ec4c3b75
moisesmsa <moisessousa241@gmail.com>
2024-05-09 20:51:56 -0300

    ajustes relatorio

--- lib/boleto.ex
+++ lib/boleto.ex
@@ -1,36 +1,12 @@
 defmodule Boleto do
-  @moduledoc """
-  Documentation for Boleto.
-  """
-
-  @doc """
-  Hello world.
-
-
-  """
-  def main do
-    IO.puts(">>>>>>>>> teste")
-    #  |>
-  end
-
-  def codigo_banco do
-    001
-  end
-
-  # def string_para_lista_numerica(string) do
-  #   # IO.inspect({string, :erlang.element(1, :erlang.type(string))})
-  #   string
-  #   |> String.graphemes()
-  #   |> Enum.map(&String.to_integer/1)
-  # end
 
   def codigo_moeda(type) do
     type =
-          if(type != "real") do
-            "outro"
-          else
-            type
-          end
+      if(type != "real") do
+        "outro"
+      else
+        type
+      end
 
     moedas = %{
       "real" => 9,
@@ -50,17 +26,13 @@ defmodule Boleto do
         [2, 1]
       end
 
-    # IO.inspect(cycle)
-
     fatores = Stream.cycle(cycle)
     fatores = Enum.take(fatores, total_digitos)
 
-    # tratar string
     soma_campo =
       String.split(campo, "", trim: true)
       |> Enum.map(&String.to_integer/1)
       |> Enum.zip(fatores)
-      ## refatorar
       |> Enum.map(fn {x, y} ->
         value = x * y
 
@@ -78,15 +50,13 @@ defmodule Boleto do
     dv = rem(proxima_dezena - resto, 10)
   end
 
+  # Adiciona ao fator de vencimento a data base e retorna a data original
   def fator_venc(fator) do
     {:ok, data_base} = Date.new(1997, 10, 7)
     data_nova = Date.add(data_base, fator)
-
-    # {year, month, day} = {data_nova.year, data_nova.month, data_nova.day}
-
-    # ao receber o fator de vencimento essa funcao adiciona ele a data base para retornar o dia, mes e ano da data de vencimento
   end
 
+  # Recebe o dia, mes e ano de vencimento subtrai a data da data base para retornar o fator de vencimento
   def fator_venc(dia, mes, ano) do
     {:ok, dataBase} = Date.new(1997, 10, 7)
 
@@ -94,8 +64,7 @@ defmodule Boleto do
 
     fator = Date.diff(dataNova, dataBase)
 
-    # ao receber o dia, mes e ano de vencimento a funcao subtrae a data da data base para retornar o fator de vencimento
-    # necessidade pois o fator de vencimento ao chegar a 10 mil retorna a mil.
+    # quando fator de vencimento chega em 10 mil deve retornar mil.
     if fator >= 10000 do
       fator - 9000
     else
@@ -103,23 +72,20 @@ defmodule Boleto do
     end
   end
 
-
   def valor_codificador(preco) do
     # arredonda o valor multiplicando por 100
     valorArredondado = round(preco * 100)
 
     valorFormatado = Integer.to_string(valorArredondado)
 
-    # transforma o valor em string para usar a funcao pad_leading para preencher o valor com zeros
+    # transforma em string e usa pad_leading para preencher o valor com zeros
     valorFinal = String.pad_leading(valorFormatado, 10, "0")
   end
 
+  # transforma valor de dez digitos em string e divide por 100 para calcular os centavos
   def valor_decodificador(valor) do
     preco = String.to_integer(valor)
-
-    IO.inspect(Float.to_string(preco / 100))
-
-    # se o valor tiver dez digitos e for do tipo binary ele transforma em uma string e divide por 100 para adicionar os centavos ao valor do codigo de barras
+    Float.to_string(preco / 100)
   end
 
   def nosso_numero_dv(campo) do
@@ -159,17 +125,16 @@ defmodule Boleto do
     fatores = Stream.cycle([2, 3, 4, 5, 6, 7, 8, 9])
 
     fatores =
-              Enum.take(fatores, total_digitos)
-              |> Enum.reverse()
+      Enum.take(fatores, total_digitos)
+      |> Enum.reverse()
 
     soma_campo =
-                String.split(campos, "", trim: true)
-                |> Enum.map(&String.to_integer/1)
-                |> Enum.zip(fatores)
-                |> Enum.map(fn {x, y} -> value = x * y end)
-                |> Enum.sum()
+      String.split(campos, "", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.zip(fatores)
+      |> Enum.map(fn {x, y} -> value = x * y end)
+      |> Enum.sum()
 
-    # só muda essa parte aqui
     resto = 11 - rem(soma_campo, 11)
 
     if resto == 0 or resto == 10 or resto == 11 do
@@ -179,6 +144,16 @@ defmodule Boleto do
     end
   end
 
+  def formata_data(data) do
+    [dia, mes, ano] = String.split(data, "/")
+
+    dia = String.to_integer(dia)
+    mes = String.to_integer(mes)
+    ano = String.to_integer(ano)
+
+    fator_venc(dia, mes, ano)
+  end
+
   def codificador do
     codigo_banco =
       IO.gets("Digite o código do banco (3 dígitos): \n")
@@ -186,26 +161,17 @@ defmodule Boleto do
       |> String.slice(0, 3)
 
     moeada =
-      IO.gets("Digite a moeda (Real ou outro - 1 dígito): \n")
+      IO.gets("Digite a moeda (Real ou outro - \"real\" ou \"outro\"): \n")
       |> String.downcase()
       |> String.trim()
 
     moeada = Boleto.codigo_moeda(moeada)
 
-    dia_venc =
-      IO.gets("Digite o dia de vencimento (dd - 2 dígitos): \n")
+    data_venc =
+      IO.gets("Digite a data de vencimento (dd/mm/yyy): \n")
       |> String.trim()
-      |> String.to_integer()
 
-    mes_venc =
-      IO.gets("Digite o mês de vecimento (mm - 2 dígitos): \n")
-      |> String.trim()
-      |> String.to_integer()
-
-    ano_venc =
-      IO.gets("Digite o ano de vecimento (yyyy - 4 dígitos): \n")
-      |> String.trim()
-      |> String.to_integer()
+    fator_v = formata_data(data_venc)
 
     valor =
       IO.gets("Digite o valor (formato: 0.00):\n")
@@ -238,64 +204,95 @@ defmodule Boleto do
       |> String.slice(0, complemento_size)
 
     nosso_numero = convenio <> complemento
-    nosso_numero = nosso_numero <> nosso_numero_dv(nosso_numero)
 
-    agencia = ""
-    conta = ""
-    # caso nosso número possua 17 dígitos
-    carteira = "21"
+    dados_bancarios =
+      # carteira, agencia e conta não são obrigatórios para convênios de tamanho 7
+      if convenio_size < 7 do
+        agencia =
+          IO.gets("Digite o número da agência (4 dígitos): \n")
+          |> String.trim()
+          |> String.slice(0, 4)
 
-    if convenio_size < 7 do
-      agencia =
-                IO.gets("Digite o número da agência (4 dígitos): \n")
-                |> String.trim()
-                |> String.slice(0, 4)
+        conta =
+          IO.gets("Digite o número da conta sem dv (8 dígitos): \n")
+          |> String.trim()
+          |> String.slice(0, 8)
 
-      conta =
-              IO.gets("Digite o número da conta sem dv (8 dígitos): \n")
-              |> String.trim()
-              |> String.slice(0, 8)
+        carteira =
+          IO.gets("Digite o número da carteira (2 dígitos): \n")
+          |> String.trim()
+          |> String.slice(0, 2)
 
-      carteira =
-                IO.gets("Digite o número da carteira (2 dígitos): \n")
-                |> String.trim()
-                |> String.slice(0, 2)
-    end
+        Enum.join([agencia, conta, carteira])
+      else
+        # caso nosso-número possua 17 dígitos a carteira possui esse valor padrão
+        carteira = "21"
+      end
 
-
-      codigo_de_barras = Enum.join([
+    codigo_de_barras =
+      Enum.join([
         codigo_banco,
         moeada,
-        fator_venc(
-          dia_venc,
-          mes_venc,
-          ano_venc
-        ),
+        fator_v,
         valor_codificador(valor),
         nosso_numero,
-        agencia,
-        conta,
-        carteira
+        dados_bancarios
       ])
 
-      dv = codigo_barras_dv(codigo_de_barras)
+    dv = codigo_barras_dv(codigo_de_barras)
 
-      codigo_de_barras = String.split_at(codigo_de_barras, 4)
-      codigo_de_barras = "#{elem(codigo_de_barras, 0)}#{dv}#{elem(codigo_de_barras, 1)}"
+    codigo_de_barras = String.split_at(codigo_de_barras, 4)
+    codigo_de_barras = "#{elem(codigo_de_barras, 0)}#{dv}#{elem(codigo_de_barras, 1)}"
 
-      Barlix.Code39.encode!(codigo_de_barras) |> Barlix.PNG.print(file: "./barcode.png")
+    IO.puts("Código de barras: #{codigo_de_barras} tamanho: #{String.length(codigo_de_barras)}")
+    # IO.puts("Linha digitável:")
+    Barlix.ITF.encode!(codigo_de_barras) |> Barlix.PNG.print(file: "./barcode.png")
   end
 
-  def decodificador do
-    # Digite linha digitavel com os 44 elementos
-    # Retorna
-    # • Linha digitável;
-    # • Código do banco;
-    # • Moeda;
-    # • Data de vencimento (dia/mes/ano, na forma DD/MM/AAAA);
-    # • valor;
-    # • tipo de convenio (04, 05, o7 posições ou livre com 17 posições). Ver https://www.bb.com.br/docs/
-    # pub/emp/empl/dwn/Doc5175Bloqueto.pdf;
-    # • Dados específicos para cada tipo de convênio
+  # Retorna
+  # • Código do banco;
+  # • Moeda;
+  # • Data de vencimento (dia/mes/ano, na forma DD/MM/AAAA);
+  # • valor;
+  # • tipo de convenio (04, 05, o7 posições ou livre com 17 posições). Ver https://www.bb.com.br/docs/
+  def decodificador(codigo_barra) do
+
+    codigo_banco = String.slice(codigo_barra,0..2)
+
+    IO.puts("Código banco #{codigo_banco}")
+    moeda = String.at(codigo_barra, 3)
+
+    if moeda == "9" do
+      IO.puts("Moeda: Real")
+    end
+
+    if moeda == "0" do
+      IO.puts("Moeda: Outro")
+    end
+
+    data_vencimento = fator_venc(String.slice(codigo_barra,5..8)
+                                |> String.to_integer())
+
+    IO.puts("Data venc: #{data_vencimento.day}/#{data_vencimento.month}/#{data_vencimento.year}")
+
+    valor = valor_decodificador(String.slice(codigo_barra,9..18))
+    IO.puts("Valor: #{valor}")
+
+    numero_convenio = String.slice(codigo_barra,19..22)
+    IO.puts("Convênio: #{numero_convenio}")
+
+    complemento = String.slice(codigo_barra,23..29)
+    IO.puts("Complemento: #{complemento}")
+
+    agencia = String.slice(codigo_barra,30..33)
+    IO.puts("Agência: #{agencia}")
+
+    conta = String.slice(codigo_barra,34..41)
+    IO.puts("Conta: #{conta}")
+
+    carteira = String.slice(codigo_barra,42..43)
+    IO.puts("Carteira: #{carteira}")
+
+    # IO.puts("Linha digitável:")
   end
 end
