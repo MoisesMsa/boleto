4249491ab4513e9653ca6c9c3a88e506fd27002d
Raphael <brsabbadobr@gmail.com>
2024-05-09 18:47:14 -0300

    sem comentarios e sem impressões de teste

--- lib/boleto.ex
+++ lib/boleto.ex
@@ -1,28 +1,12 @@
 defmodule Boleto do
-  @moduledoc """
-  Documentation for Boleto.
-  """
 
-  @doc """
-  Hello world.
-
-
-  """
   def main do
     IO.puts(">>>>>>>>> teste")
-    #  |>
   end
 
   def codigo_banco do
     001
   end
-
-  # def string_para_lista_numerica(string) do
-  #   # IO.inspect({string, :erlang.element(1, :erlang.type(string))})
-  #   string
-  #   |> String.graphemes()
-  #   |> Enum.map(&String.to_integer/1)
-  # end
 
   def codigo_moeda(type) do
     moedas = %{
@@ -33,7 +17,7 @@ defmodule Boleto do
     moedas[type]
   end
 
-  ## moises
+
   def dv_line(campo) do
     total_digitos = String.length(campo)
 
@@ -44,17 +28,17 @@ defmodule Boleto do
         [2, 1]
       end
 
-    # IO.inspect(cycle)
+
 
     fatores = Stream.cycle(cycle)
     fatores = Enum.take(fatores, total_digitos)
 
-    # tratar string
+
     soma_campo =
       String.split(campo, "", trim: true)
       |> Enum.map(&String.to_integer/1)
       |> Enum.zip(fatores)
-      ## refatorar
+
       |> Enum.map(fn {x, y} ->
         value = x * y
 
@@ -82,9 +66,6 @@ defmodule Boleto do
 
     Date.to_iso8601(data_nova)
 
-    #{year, month, day} = {data_nova.year, data_nova.month, data_nova.day}
-    #ao receber o fator de vencimento essa funcao adiciona ele a data base para retornar o dia, mes e ano da data de vencimento
-
   end
 
   def fator_venc(dia, mes, ano) do
@@ -94,8 +75,7 @@ defmodule Boleto do
 
     fator = Date.diff(dataNova, dataBase)
 
-    #ao receber o dia, mes e ano de vencimento a funcao subtrae a data da data base para retornar o fator de vencimento
-    #necessidade pois o fator de vencimento ao chegar a 10 mil retorna a mil.
+
     if fator >= 10000 do
       fator - 9000
     else
@@ -105,16 +85,16 @@ defmodule Boleto do
     fator
   end
 
-  ### rapahel
+
 
   def valor_codificador(preco) do
 
-    #arredonda o valor multiplicando por 100
+
     valorArredondado = round(preco * 100)
 
     valorFormatado = Integer.to_string(valorArredondado)
 
-    #transforma o valor em string para usar a funcao pad_leading para preencher o valor com zeros
+
     valorFinal = String.pad_leading(valorFormatado, 10, "0")
 
     valorFinal
@@ -127,10 +107,10 @@ defmodule Boleto do
 
     Float.to_string(preco/100)
 
-    #se o valor tiver dez digitos e for do tipo binary ele transforma em uma string e divide por 100 para adicionar os centavos ao valor do codigo de barras
+
   end
 
-  ## moises
+
   def nosso_numero(campo) do
     total_digitos = String.length(campo)
 
@@ -140,7 +120,7 @@ defmodule Boleto do
       Enum.take(fatores, total_digitos)
       |> Enum.reverse()
 
-    # tratar string
+
     soma_campo =
       String.split(campo, "", trim: true)
       |> Enum.map(&String.to_integer/1)
@@ -161,33 +141,10 @@ defmodule Boleto do
       end
   end
 
-  ### moises
+
   def complemento_do_num do
   end
 
-  # ### aleatorio
-  # def num_convenio do
-  #   # caso 1 CCCCC
-  #   # caso 2 CCCCCc
-  #   # caso 3 CCCCCCC
-  #   # caso 4 livre do cliente
-  # end
-
-  # ### aleatorio
-  # def num_agencia do
-  # end
-
-  # ### aleatorio
-  # def conta_corrente do
-  # end
-
-  # ### aleatorio
-  # def tipo_carteria do
-  # end
-
-  # def linha(digitavel) do
-  #   Enum.join([codigo_banco, codigo_moeda, fator_venc, valor, nosso_numero, num_convenio, complemento_do_num, num_agencia, conta_corrente, tipo_carteria])
-  # end
 
   def formata_data(data) do
     [dia, mes, ano] = String.split(data, "/")
@@ -196,14 +153,7 @@ defmodule Boleto do
   end
 
   def codificador(moeda, data, valor) do
-    # Digite o
-    # • Código do banco;
-    # • Moeda;
-    # • Data de vencimento (dia/mes/ano, na forma DD/MM/AAAA);
-    # • valor;
-    # • tipo de convenio (04, 05, o7 posições ou livre com 17 posições). Ver https://www.bb.com.br/docs/ pub/emp/empl/dwn/Doc5175Bloqueto.pdf;
-    # • Dados específicos para cada tipo de convênio
-    #  retorna saida codigo de barras e linha digitavel
+
 
     [dia, mes, ano] = formata_data(data)
     codigo_banco = codigo_banco()
@@ -211,7 +161,7 @@ defmodule Boleto do
     fator_venc = fator_venc(dia, mes, ano)
     valor = valor_codificador(valor)
 
-    #entre codigo moeda e fator vencimento tem que adicionar dv do codigo de barras e após valores são apenas numeros aleatorios.
+
     code = Enum.join([codigo_banco, codigo_moeda, fator_venc, valor, nosso_numero, num_convenio, complemento_do_num, num_agencia, conta_corrente, tipo_carteria])
   end
 
@@ -220,16 +170,6 @@ defmodule Boleto do
   end
 
   def decodificador(codigo_barra) do
-    # Digite linha digitavel com os 44 elementos
-    # Retorna
-    # • Linha digitável;
-    # • Código do banco;
-    # • Moeda;
-    # • Data de vencimento (dia/mes/ano, na forma DD/MM/AAAA);
-    # • valor;
-    # • tipo de convenio (04, 05, o7 posições ou livre com 17 posições). Ver https://www.bb.com.br/docs/
-    # pub/emp/empl/dwn/Doc5175Bloqueto.pdf;
-    # • Dados específicos para cada tipo de convênio
 
 
     codigo_banco = String.slice(codigo_barra,0..2)
